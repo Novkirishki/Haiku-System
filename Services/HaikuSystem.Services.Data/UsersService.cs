023c344d80a54d5ae521643d359e8d4e3af64e78
Novkirishki <novkirishki94@gmail.com>
2016-02-13 12:46:53 +0200

    Fixed users ordering

--- Services/HaikuSystem.Services.Data/UsersService.cs
+++ Services/HaikuSystem.Services.Data/UsersService.cs
@@ -31,10 +31,17 @@
                     switch (sortType)
                     {
                         case "ascending":
-                            result = result.OrderBy(u => u.Username);
+                            result = result.OrderByDescending(u => u.IsVIP)
+                                .ThenBy(u => u.Username)
+                                .Skip(skip)
+                                .Take(take);
                             break;
                         case "descending":
-                            result = result.OrderByDescending(u => u.Username);
+                            result = result
+                                .OrderByDescending(u => u.IsVIP)
+                                .ThenByDescending(u => u.Username)
+                                .Skip(skip)
+                                .Take(take);
                             break;
                     }
                     break;
@@ -42,16 +49,24 @@
                     switch (sortType)
                     {
                         case "ascending":
-                            result = result.OrderBy(u => u.Haikus.Count == 0 ? 0 : ((double)u.Haikus.Sum(h => h.Ratings.Count == 0 ? 0 : h.Ratings.Sum(r => r.Value)) / (u.Haikus.Sum(h => h.Ratings.Count) == 0 ? 1 : u.Haikus.Sum(h => h.Ratings.Count))));
+                            result = result
+                                .OrderByDescending(u => u.IsVIP)
+                                .ThenBy(u => u.Haikus.Count == 0 ? 0 : ((double)u.Haikus.Sum(h => h.Ratings.Count == 0 ? 0 : h.Ratings.Sum(r => r.Value)) / (u.Haikus.Sum(h => h.Ratings.Count) == 0 ? 1 : u.Haikus.Sum(h => h.Ratings.Count))))
+                                .Skip(skip)
+                                .Take(take);
                             break;
                         case "descending":
-                            result = result.OrderBy(u => u.Haikus.Count == 0 ? 0 : ((double)u.Haikus.Sum(h => h.Ratings.Count == 0 ? 0 : h.Ratings.Sum(r => r.Value)) / (u.Haikus.Sum(h => h.Ratings.Count) == 0 ? 1 : u.Haikus.Sum(h => h.Ratings.Count))));
+                            result = result
+                                .OrderByDescending(u => u.IsVIP)
+                                .ThenByDescending(u => u.Haikus.Count == 0 ? 0 : ((double)u.Haikus.Sum(h => h.Ratings.Count == 0 ? 0 : h.Ratings.Sum(r => r.Value)) / (u.Haikus.Sum(h => h.Ratings.Count) == 0 ? 1 : u.Haikus.Sum(h => h.Ratings.Count))))
+                                .Skip(skip)
+                                .Take(take);
                             break;
                     }
                     break;
             }
-
-            return result.OrderByDescending(u => u.IsVIP).Skip(skip).Take(take);
+            
+            return result;
         }
 
         public IQueryable<User> GetByPublishCode(string publishCode)
